549b2cf09039b34e83f4f496c677f2c31a5246c3
merradd <165909244+merradd@users.noreply.github.com>
2024-12-16 21:21:33 +0300

    Revert "Van branch"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <section className="page w-full">
       <Link href={'/news'}>To news</Link>
-      <Link href={'/institute'}>To institute</Link>
-      <Link href={'/dalian'}>To dalian</Link>
     </section>
   );
 }
--- src/components/contacts_page_components/Title.tsx
+++
@@ -1,7 +0,0 @@
-export default function Title() {
-  return (
-    <div className='main__text_h1'>
-      <h1>Контакты</h1>
-    </div>
-  );
-}
